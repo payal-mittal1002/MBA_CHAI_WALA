782fb027b93342d2be7e3881ad647cf87ab9361c
Payal Mittal <payal8949531232@gmail.com>
2023-06-26 20:00:38 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,194 @@
-import logo from './logo.svg';
-import './App.css';
+import IntroVideo from "./components/IntroVideo";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
+import Misc from "./components/Misc";
 
+import "./Styles/App.scss"
+import "./Styles/Intro.scss"
+import "./Styles/Section.scss"
+import "./Styles/Color.scss"
+import "./Styles/Footer.scss" 
+import "./Styles/Misc.scss"
+
+import "./Styles/mediaQuery.scss"
+
+import freshTopicImg from "./assets/academy.png"
+import freshTopic2Img from "./assets/story.png"
+import tedTalksImg from "./assets/in-the-news.gif"
+import franchiseImg from "./assets/franchise.gif"
+import mapImg from "./assets/locations.png"
+import coursesImg from "./assets/image2.png"
+import albumImg from "./assets/mba-cares.gif"
+import baratImg from "./assets/image1.png"
+import chaiwalaImg from "./assets/image3.png"
+
+
+import data from "./data/data.json"
+import { useEffect } from "react";
+
+
+const yellow="#fff100",
+pink="#ed1e79",
+red=":#d20120",
+white="#fff",
+brown="#6d3d0f";
 function App() {
+
+  const {freshTopic,freshTopic2,tedTalks,franchise,map,courses,album,barat,chaiwala }=data;
+  
+  const dotCursor=(e)=>{
+      const cursor=document.querySelector(".cursor");
+      
+      cursor.style.top = `${e.pageY - 14}px`
+      cursor.style.left = `${e.pageX - 14}px`
+      const element=e.target;
+      if(element.getAttribute("data-cursorpointer")){
+          cursor.classList.add("cursorHover")
+      }
+      else if(element.getAttribute("data-cursorpointermini")){
+          cursor.classList.add("cursorHoverMini")
+      }
+      else{
+          cursor.classList.remove("cursorHover")
+          cursor.classList.remove("cursorHoverMini")
+      }
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove",dotCursor);
+    return () => {
+      window.removeEventListener("mousemove",dotCursor);
+    };
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <IntroVideo/>
+     <Section 
+     h3={freshTopic.heading} 
+     text={freshTopic.text}
+     btn={freshTopic.btn}
+     img={freshTopicImg} 
+     backgroundColor={pink}
+     headingColor={yellow}
+     textColor={yellow}
+     btnBgColor={yellow}
+     btnColor={pink}
+     />
+
+
+<Section 
+     h3={freshTopic2.heading} 
+     text={freshTopic2.text}
+     btn={freshTopic2.btn}
+     img={freshTopic2Img} 
+     backgroundColor={pink}
+     headingColor={yellow}
+     textColor={yellow}
+     btnBgColor={yellow}
+     btnColor={pink}
+     />
+
+
+
+<Section 
+     h3={tedTalks.heading} 
+     text={tedTalks.text}
+     btn={tedTalks.btn}
+     img={tedTalksImg} 
+     backgroundColor={yellow}
+     headingColor={pink}
+     textColor={pink}
+     btnBgColor={pink}
+     btnColor={yellow}
+     />
+
+
+<Section 
+     h3={franchise.heading} 
+     text={franchise.text}
+     btn={franchise.btn}
+     img={franchiseImg} 
+     backgroundColor={white}
+     headingColor={pink}
+     textColor={brown}
+     btnBgColor={brown}
+     btnColor={yellow}
+     />
+
+
+<Section 
+     h3={map.heading} 
+     text={map.text}
+     img={mapImg} 
+     backgroundColor={pink}
+     headingColor={yellow}
+     textColor={yellow}
+     btnBgColor={brown}
+     btnColor={yellow}
+     hasbtn={false}
+     />
+
+
+<Section 
+     h3={courses.heading} 
+     text={courses.text}
+     btn={courses.btn}
+     img={coursesImg} 
+     imgsize="30%"
+     backgroundColor={yellow}
+     headingColor={pink}
+     textColor={pink}
+     btnBgColor={pink}
+     btnColor={yellow}
+     />
+
+
+<Section 
+     h3={album.heading} 
+     text={album.text}
+     btn={album.btn}
+     img={albumImg} 
+     backgroundColor={white}
+     headingColor={pink}
+     textColor={brown}
+     btnBgColor={brown}
+     btnColor={yellow}
+     />
+
+
+
+<Section 
+     h3={barat.heading} 
+     text={barat.text}
+     btn={barat.btn}
+     img={baratImg} 
+     backgroundColor={pink}
+     headingColor={yellow}
+     textColor={yellow}
+     btnBgColor={yellow}
+     btnColor={pink}
+     />
+
+
+
+<Section 
+     h3={chaiwala.heading} 
+     text={chaiwala.text}
+     btn={chaiwala.btn}
+     img={chaiwalaImg} 
+     backgroundColor={white}
+     headingColor={pink}
+     textColor={brown}
+     btnBgColor={brown}
+     btnColor={yellow}
+     />
+  
+
+
+    <Footer/>
+    <Misc/>
+    </>
+
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const Footer = () => {
+    const footerLinks=(element)=>{
+        const allfooter=document.querySelectorAll(".footerLinks");
+        console.log(allfooter);
+        switch (element) {
+            case 0:
+                allfooter.forEach((item,index)=>(index===0)?(item.style.opacity=1):(item.style.opacity=0.2));
+                break;
+            case 1:
+                allfooter.forEach((item,index)=>(index==1)?(item.style.opacity=1):(item.style.opacity=0.2));
+                break;
+            case 2:
+                allfooter.forEach((item,index)=>(index===2)?(item.style.opacity=1):(item.style.opacity=0.2));
+                break;
+            case 3:
+                allfooter.forEach((item,index)=>(index===3)?(item.style.opacity=1):(item.style.opacity=0.2));
+                break;
+            case 4:
+                allfooter.forEach((item,index)=>(index===4)?(item.style.opacity=1):(item.style.opacity=0.2));
+                break;
+            case 5:
+                allfooter.forEach((item,index)=>(index===5)?(item.style.opacity=1):(item.style.opacity=0.2));
+                break;   
+            default:
+                allfooter.forEach((item,index)=>(item.style.opacity=1));
+                break;
+        }
+    }
+    const linksNormal=()=>{
+        const allfooter=document.querySelectorAll(".footerLinks");
+        allfooter.forEach((item,index)=>(item.style.opacity=1));
+    }
+  return (
+   <>
+   <footer>
+    <h1>
+        let's
+        <br/>
+        #connectOnCutting?
+    </h1>
+    <aside onMouseLeave={linksNormal}>
+        <a href="/" className='footerLinks' onMouseOver={()=>footerLinks(0)} data-cursorpointermini={true}>Home</a>
+        <a href="/story" className='footerLinks' onMouseOver={()=>footerLinks(1)} data-cursorpointermini={true}>story</a>
+        <a href="/media" className='footerLinks' onMouseOver={()=>footerLinks(2)} data-cursorpointermini={true}>Media</a>
+        <a href="/franchise" className='footerLinks' onMouseOver={()=>footerLinks(3)} data-cursorpointermini={true}>Franchise</a>
+        <a href="/event" className='footerLinks' onMouseOver={()=>footerLinks(4)} data-cursorpointermini={true}>Event</a>
+        <a href="/chaiwalacares" className='footerLinks' onMouseOver={()=>footerLinks(5)} data-cursorpointermini={true}>ChaiWalacares</a>
+    </aside>
+    <div>
+        <h5>Email</h5>
+        <a href=" mailto: info@mbachaiwala.com">Info@mbachaiwala.com</a>
+        <a href=" mailto: franchise@mbachaiwala.com">franchise@mbachaiwala.com</a>
+    </div>
+    <div>
+        <h5>Phone</h5>
+        <a href=" tel: +91722905222">+91722905222</a>
+    
+    </div>
+    <p>© COPYRIGHT 2023 MBA CHAIWALA.MADE BY </p>
+   </footer>
+   <div className='Footer'></div>
+   </>
+  )
+}
+
+export default Footer
